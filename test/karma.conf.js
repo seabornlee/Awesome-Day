cd10f0741e07b60500c079044779f7d687d72c8a
Seaborn Lee <seabornlee.cn@gmail.com>
2015-02-12 20:19:10 +0800

    add bootflat theme

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,6 +29,9 @@ module.exports = function(config) {
       'bower_components/angular-ui-sortable/sortable.js',
       'bower_components/angular-xeditable/dist/js/xeditable.js',
       'bower_components/angular-timer/dist/angular-timer.js',
+      'bower_components/Bootflat/bootflat/js/icheck.min.js',
+      'bower_components/Bootflat/bootflat/js/jquery.fs.selecter.min.js',
+      'bower_components/Bootflat/bootflat/js/jquery.fs.stepper.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
